69347a0f2febd8c82620b9409ed53ade70cc5fb8
federica.merola90 <federica.merola90@outlook.it>
2021-04-01 16:13:58 +0200

    First push

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using API.Entities;
+using API.Data;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // api/users/[id]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        [HttpGet("syncallusers")]
+        public ActionResult<IEnumerable<AppUser>> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("synconeuser/{id}")]
+        public ActionResult<AppUser> GetUserSync(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
