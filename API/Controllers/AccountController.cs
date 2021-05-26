78e398226cacf651564658513131896850fa8ba1
federica.merola90 <federica.merola90@outlook.it>
2021-05-26 15:08:07 +0200

    Part of Section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -27,7 +27,7 @@ namespace API.Controllers
 
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-        {
+        {   
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken!");
 
             var user = _mapper.Map<AppUser>(registerDto);
@@ -45,7 +45,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -72,7 +73,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
